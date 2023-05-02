fa41529fbfe258cd3e8645c152a3a9f62744f825
karim-hammami <hammamik790@gmail.com>
2023-05-02 03:39:05 +0100

    working on analysis-service

--- analysis-service/main.py
+++ analysis-service/main.py
@@ -1,21 +1,57 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+from gridfs import GridFS
+from bson import ObjectId
+import librosa
+import numpy as np
+
+
+
+# MongoDB connection details
+mongo_uri = "mongodb://localhost:32768"
+database_name = "StreamDB"
 
 # Connect to MongoDB
-client = MongoClient("mongodb://localhost:32768")
-db = client["StreamDB"]
-fs = db["fs.files"]
+client = MongoClient(mongo_uri)
+database = client[database_name]
+fs = GridFS(database)
 
-def get_file_details(file_id):
-    file_doc = fs.find_one({"_id": ObjectId(file_id)})
-    if file_doc:
-        filename = file_doc.get("filename")
-        filesize = file_doc.get("length")
-        print("File Name: ", filename)
-        print("File Size: ", filesize, "bytes")
+# ObjectId of the file in GridFS
+file_object_id = ObjectId("64440a2efe7e7a2bee86f332")
+
+def calculate_acousticness(file_object_id):
+    # Retrieve the file from GridFS
+    file_object = fs.find_one({"_id": file_object_id})
+
+    if file_object:
+        # Get the filename
+        filename = file_object.filename
+
+        # Load audio data using LibROSA
+        audio, sr = librosa.load(file_object)
+
+        # Compute the acousticness feature
+        acousticness = librosa.feature.spectral_centroid(y=audio, sr=sr)
+
+        # Calculate the average acousticness
+        average_acousticness = np.mean(acousticness)
+
+        # Scale the average acousticness between 0 and 1
+        scaled_acousticness = (average_acousticness - np.min(acousticness)) / (np.max(acousticness) - np.min(acousticness))
+
+        # Format the scaled acousticness to three decimal places
+        formatted_acousticness = round(scaled_acousticness, 3)
+
+        # Return the filename and formatted acousticness
+        return filename, formatted_acousticness
     else:
-        print("File not found.")
+        return None, None
 
-# Usage example
-file_id = "6449321899fb4332808713c1"
-get_file_details(file_id)
+# Call the function and get the result
+filename, acousticness = calculate_acousticness(file_object_id)
+
+# Print the result
+if filename and acousticness is not None:
+    print("Filename:", filename)
+    print("Acousticness:", acousticness)
+else:
+    print("File not found.")
